7b001518f276ff76f9eea14ac54b09ed714cbb18
raviparmar9983 <parmarravi1162@gmail.com>
2025-05-11 17:23:53 +0530

    refactor: change in api url

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
